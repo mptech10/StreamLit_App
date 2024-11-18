0e3ae6e91769801eeb97ffc8df84065ef003f83e
mptech10 <megan.inferyx@gmail.com>
2024-11-18 13:16:14 -0500

    real_format

--- App/formats.py
+++ App/formats.py
@@ -115,7 +115,7 @@ def getPath(node, nodes, path=""):
 
 # validate at https://toolkitbay.com/tkb/tool/csv-validator
 filename = utils.getFullPath("data/employee-manager.csv")
-df = pd.read_csv(filename1, header=0).convert_dtypes()
+df = pd.read_csv(filename, header=0).convert_dtypes()
 #chnaged this
 
 # convert and save as JSON
